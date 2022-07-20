5c34d43df5b1192afcea2f06bbf6757da4a3157d
shtrumj@gmail.com <piano74672@>
2022-07-20 07:01:32 +0300

    WithFunctionalAPI

--- taskManager/static/EditCustomerts.js
+++ taskManager/static/EditCustomerts.js
@@ -1,51 +1,34 @@
-// // window.addEventListener('DOMContentLoaded', (event) => {
-// //     const select = document.getElementById("customer");
-// //     console.log(select.value)});
-// window.addEventListener('DOMContentLoaded', (event) => {
-//     clientname= document.getElementById("name");
-//     clientcity= document.getElementById("city");
-//     clientadd = document.getElementById("address");
-//     clientind= document.getElementById("internalDomain");
-//     clientexd= document.getElementById("externalDomain");
-//     clientowadd= document.getElementById("owaadd");
-//     submit = document.getElementById("submit");
-//     clientname.value += custname
-//     clientcity.value += custcity
-//     clientadd.value += custadd
-//     clientind.value += custindomain
-//     clientexd.value += custexdomain
-//     clientowadd.value += custowaadd
-//     submit.value = 'ערוך לקוח'
-// });
 function myfunc(){
-    getCust()
-    select = document.getElementById("mycustomer");
-    console.log(select.value);
-    clientname= document.getElementById("name");
-    clientcity= document.getElementById("city");
-    clientadd = document.getElementById("address");
-    clientind= document.getElementById("internalDomain");
-    clientexd= document.getElementById("externalDomain");
-    clientowadd= document.getElementById("owaadd");
+    cust_fetch();
+    let cselect = document.getElementById("mycustomer");
+    let cvalue= cselect.value;
+    let cvalue1 = cselect.options[cselect.selectedIndex].text
+    let clientid= document.getElementById("cid");
+    let clientname= document.getElementById("name");
+    let clientcity= document.getElementById("city");
+    let clientadd = document.getElementById("address");
+    let clientind= document.getElementById("internalDomain");
+    let clientexd= document.getElementById("externalDomain");
+    let clientowadd= document.getElementById("owaadd");
     clientname.value += custname
+    clientid.value += custid
     clientcity.value += custcity
     clientadd.value += custadd
     clientind.value += custindomain
     clientexd.value += custexdomain
     clientowadd.value += custowaadd
-    console.log(custcity);
+    // console.log(select.value);
+};
+async function cust_fetch() {
+    const response = await fetch(
+        'http://127.0.0.1:8765/it',
+        {
+            method: 'GET'
+        }
+);
+    const data = await resonse.json();
+    consol.log(data)
 }
-async function getCust(){
-     const api_url = 'http://127.0.0.1:8765/it'
-     const response = await fetch(api_url, {});
-     const data = await response.json();
-     clientname.value = data[select.value -1].name;
-     clientcity.value = data[select.value -1].city;
-     clientadd.value = data[select.value -1].address;
-     clientind.value = data[select.value -1].internalDomain;
-     clientexd.value = data[select.value -1].externalDomain;
-     clientowadd.value = data[select.value -1].owaAdd;
 
 
 
-}
