122b4a5699844a1b52031ed929186d4373f0d6cc
jonathan <shtrumj@gmail.com>
2022-08-08 16:39:36 +0300

    WithSVG

--- taskManager/static/js/views/tikatar3.js
+++ taskManager/static/js/views/tikatar3.js
@@ -28,13 +28,17 @@ async function servers2customers(customerSelection){
 
 function construct(HypersIP){
     let hypersIP = HypersIP
-    let counter = 0Gib$0n579
+    let counter = 0;
     const canvas = d3.select(".canvas");
-    const svg = canvas.append('svg');
-    hypersIP.forEach(function (hypercon){
+    const svg = canvas.append('svg')
+        svg.attr("width","100%").attr("height","1000")
+    hypersIP.forEach(function (hypercon) {
         const rect = svg.append('rect');
-        rect.attr('width',200).attr('height',200).attr('fill','blue').attr('x',20).attr('y',20)
-
-
+        rect.attr('width', 200).attr('height', 200).attr('fill', 'green').attr('x', 0 + counter).attr('y', 20).attr("rx","20").attr("preserveAspectRatio","xMaxYMin meet")
+        const text = svg.append('text');
+        text.attr('fill', 'black').attr('x', 0).attr('y', 0).text(hypercon).attr('font-size', "55")
+        counter += 400
+        console.log(counter)
     })
+
 }
