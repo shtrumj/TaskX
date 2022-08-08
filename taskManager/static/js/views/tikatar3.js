7666175123052e3e0e0919211a5378eeab3a6e7c
shtrumj@gmail.com <piano74672@>
2022-08-08 10:26:36 +0300

    change layout

--- taskManager/static/js/views/tikatar3.js
+++ taskManager/static/js/views/tikatar3.js
@@ -20,9 +20,21 @@ async function servers2customers(customerSelection){
         )
     const ServersPerCustomerData = await ServersResponse.json();
     ServersPerCustomerData['data'].forEach(function (item){
-        HypersPerCustomer.push(item["hyper_id"])
+        HypersPerCustomer.push(item["hyper_ip"])
         })
     let uniqueHypersPerCustomer = [...new Set(HypersPerCustomer)]
-    console.log(uniqueHypersPerCustomer)
-
+     construct(uniqueHypersPerCustomer)
 };
+
+function construct(HypersIP){
+    let hypersIP = HypersIP
+    let counter = 0Gib$0n579
+    const canvas = d3.select(".canvas");
+    const svg = canvas.append('svg');
+    hypersIP.forEach(function (hypercon){
+        const rect = svg.append('rect');
+        rect.attr('width',200).attr('height',200).attr('fill','blue').attr('x',20).attr('y',20)
+
+
+    })
+}
